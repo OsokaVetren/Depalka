867d633fba5893dbea94afa18b5f30f1245bc3f8
Leonid Dvornikov <dvornikov.leonid.d@gmail.com>
2025-08-10 02:08:54 +0300

    add user validation + implement eballs delta

--- bd_handler.py
+++ bd_handler.py
@@ -1,4 +1,5 @@
 from sqlalchemy import create_engine, text
+from sqlalchemy.exc import IntegrityError
 from Config.config_reader import config
 
 conn_str = (
@@ -8,14 +9,36 @@ conn_str = (
 )
 engine = create_engine(conn_str)
 
-def isUserValid():
-    #implement validation function
-    return
+def is_user_valid(username, password):
+    query = text("""
+                 SELECT 1
+                 FROM users
+                 WHERE username = :username
+                 AND password = crypt(:password, password)
+                 LIMIT 1
+                 """)
+    with engine.connect() as conn:
+        result = conn.execute(query, {"username": username, "password": password}).first()
+    return result is not None
 
-def NewUser():
-    #implement user addition
-    return
+def new_user(username, password):
+    query = text("""
+                 INSERT INTO users (username, password)
+                 VALUES (:username, crypt(:password, gen_salt('bf')))
+                 """)
+    try:
+        with engine.begin() as conn:
+            conn.execute(query, {"username": username, "password": password})
+        return True
+    except IntegrityError:
+        return False
 
-def eballs_change():
-    #implement points change
-    return
+def eballs_change(username, delta):
+    query = text("""
+                 UPDATE users
+                 SET eballs = eballs + :delta
+                 WHERE username = :username
+                 """)
+    with engine.begin() as conn:
+        result = conn.execute(query, {"username": username, "delta": delta})
+    return result.rowcount > 0
